a04bf38a24736b828d709940b9da43611e1303f4
FranzFQ <quemejavi@gmail.com>
2026-02-26 08:53:30 -0600

    fix(infrastructure): Creating a coneccion for mongo

--- src/catalog/product/infrastructure/mongo-product-repository.ts
+++ src/catalog/product/infrastructure/mongo-product-repository.ts
@@ -4,8 +4,32 @@ import {MongoClient, Db} from "mongodb";
 
 export class MongoProductRepository implements ProductRepository {
   async save(data: Product): Promise<void> {
-    const uri = "mongodb+srv://quemejavi_db_user:JD49veU4A7hrmxDh@abasto-plus.rx4w2j6.mongodb.net/?appName=Abasto-Plus"; // o tu connection string
+    const uri = process.env.DB_MONGO || "";
+    if (!uri) {
+      throw new Error("MongoDB URI is not defined in environment variables");
+    }
     const client = new MongoClient(uri);
-    console.log("Saving product to MongoDB:", client);
+
   }
+
+  private async Insert(client: MongoClient, data: Product): Promise<void> {
+  try {
+    const db = await this.connect(client);
+
+    const coleccion = db.collection("Products");
+    await coleccion.insertOne(data);
+
+    const usuarios = await coleccion.find({}).toArray();
+    console.log(usuarios);
+  } finally {
+    await client.close();
+  }
+  }
+
+  public async connect(client: MongoClient): Promise<Db> {
+    await client.connect();
+    console.log("Conectado a MongoDB");
+    return client.db("mi_base_de_datos");
+  }
+
 }
